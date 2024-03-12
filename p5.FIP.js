af0cebc421bc315fa0159ae7d7bef0e947308723
prontopablo <darraghnolan123@gmail.com>
2024-03-12 12:33:32 +0000

    Fixed sharpen coordinates - using vertex tex coordinates now.

--- p5.FIP.js
+++ p5.FIP.js
@@ -1209,16 +1209,14 @@ const fip = {
         uniform float sharpness;
         
         void main() {
-            vec2 st = gl_FragCoord.xy / resolution;
-        
             // Center pixel
-            vec4 centerColor = texture2D(texture, st);
+            vec4 centerColor = texture2D(texture, vTexCoord);
         
             // Neighboring pixels
-            vec4 topColor = texture2D(texture, st + vec2(0.0, 1.0) / resolution);
-            vec4 bottomColor = texture2D(texture, st + vec2(0.0, -1.0) / resolution);
-            vec4 leftColor = texture2D(texture, st + vec2(-1.0, 0.0) / resolution);
-            vec4 rightColor = texture2D(texture, st + vec2(1.0, 0.0) / resolution);
+            vec4 topColor = texture2D(texture, vTexCoord + vec2(0.0, 1.0) / resolution);
+            vec4 bottomColor = texture2D(texture, vTexCoord + vec2(0.0, -1.0) / resolution);
+            vec4 leftColor = texture2D(texture, vTexCoord + vec2(-1.0, 0.0) / resolution);
+            vec4 rightColor = texture2D(texture, vTexCoord + vec2(1.0, 0.0) / resolution);
         
             // Calculate the sharpened color
             vec4 sharpenedColor = centerColor * (1.0 + 4.0 * sharpness) - sharpness * (topColor + bottomColor + leftColor + rightColor);
