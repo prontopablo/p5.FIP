5cb43be4daa31ebcaab6f2601d6a10c43311e344
prontopablo <55544101+prontopablo@users.noreply.github.com>
2025-04-28 12:32:42 +0100

    Fix all glitch example not working for the first frame

--- examples/glitch/sketch.js
+++ examples/glitch/sketch.js
@@ -4,20 +4,23 @@
 
 let bird, glitch;
 
+function preload() {
+  bird = loadImage("bird.jpg"); // *** ADD YOUR IMAGE HERE ***
+}
+
 function setup() {
   createCanvas(600, 600, WEBGL); // Use WEBGL mode to use the shader
   glitch = createFilterShader(fip.glitch); // Load the glitch shader
-  bird = loadImage("bird.jpg");
 }
   
 function draw() {
   background(0);
   imageMode(CENTER);
   image(bird, 0, 0, width, height);
+  
+  // Set the shader uniforms
+  glitch.setUniform('glitchIntensity', 0.8); // Set the intensity of the glitch effect
     
   // Apply the shader
   filter(glitch);
-    
-  // Set the shader uniforms
-  glitch.setUniform('glitchIntensity', 0.8); // Set the intensity of the glitch effect
-}
+}  
